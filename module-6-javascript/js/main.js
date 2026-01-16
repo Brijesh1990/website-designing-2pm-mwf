531b7b4e651f070e0ab6572bfa6d2b309dcb052b
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-01-16 15:52:21 +0530

    16-01-2026 all data uploaded

--- module-6-javascript/js/main.js
+++ module-6-javascript/js/main.js
@@ -1,3 +1,30 @@
 
-alert('Hi i am just check external javascript')
-window.location='https://www.tpointtech.com/'
+// alert('Hi i am just check external javascript')
+// window.location='https://www.tpointtech.com/'
+
+// calculations 
+
+function add(a,b)
+{
+    var c=a+b;
+    document.write("THe additions of numbers is :"+c)
+}
+
+function subs(a,b)
+{
+    var c=a-b;
+    document.write("THe substractions of numbers is :"+c)
+}
+
+function dv(a,b)
+{
+    var c=a/b;
+    document.write("THe divisions of numbers is :"+c)
+}
+
+
+function mult(a,b)
+{
+    var c=a*b;
+    document.write("THe multiplications of numbers is :"+c)
+}
